9276b000bf0b51423a901a83539d2aca5fb96554
feederbox826 <feederbox826@users.noreply.github.com>
2024-05-13 00:09:11 -0400

    move from plugins repo

---
+++ requires/gql-intercept.js
@@ -0,0 +1,43 @@
+// monkeypatch window fetch to intercept graphQL requests
+const { fetch: originalFetch } = unsafeWindow
+
+// gqlListener.addEventListener('response', (e) => console.log(e.detail))
+
+const gqlListener = new EventTarget()
+
+const interceptors = []
+
+unsafeWindow.fbox826 = {
+  gqlListener,
+  interceptors
+}
+
+const preCheck = (response) =>
+  response.headers.get("Content-Type") == "application/json" &&
+  response.url.endsWith("/graphql")
+
+const responseInterceptor = async (response) => {
+  // skip if not ok or not a graphql request
+  if (!response.ok || !preCheck(response)) return response
+  // get response data
+  let data = await response.clone().json()
+  // run interceptors
+  for (const interceptor of interceptors) data = await interceptor(data)
+  // trigger response event
+  gqlListener.dispatchEvent(new CustomEvent("response", { detail: data }))
+  // return response
+  return new Response(JSON.stringify(data), {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers
+  })
+}
+
+unsafeWindow.fetch = async (...args) => {
+  const [request, config] = args
+  // request interceptor here
+  const response = await originalFetch(request, config)
+  // response interceptor
+  try { return await responseInterceptor(response) }
+  catch (e) { return response }
+}
