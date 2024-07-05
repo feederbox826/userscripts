b6801b0390d060f080b90fd30286cd7d3d4d31fb
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-05 01:42:36 -0400

    add stashdb-link-chip

--- README.md
+++ README.md
@@ -6,5 +6,22 @@
 [stashdb-diff](https://github.com/feederbox826/plugins/raw/main/userscript/stashdb-diff.user.js)
 - adds text diff to stashdb detail change boxes
 
+[stashdb-link-chip](https://github.com/feederbox826/plugins/raw/main/userscript/stashdb-link-chip.user.js)
+- Creates info chips from generic `/edit/xyz`, `/studio/xyz`, `scene/xyz` links
+- add fingerprint count to scenes/ studios/ performers pending deletion as `| x FPs / x Total`, with FPs being the number of unique fingerprints and Total being the total number of submissions
+- adds chip to linked edits showing information at a glance in the format `Operation, Type • Vote Status, Current Votes, Your Vote • edit ID`
+    - Operation: ✨ Create, 🛠️ Modify, 🗑️ Delete, 🔗 Merge
+    - Type: 🏷️ Tag, 🎞️ Scene, 🎬 Studio, 🎭 Performer
+    - Vote Status: ⏳ Pending, ✅ Accepted, ❌ Rejected, 🚫 Cancelled, ⚠️ Failed
+    - Current votes: # of votes, positive or negative
+    - Your Vote: 👍 Yes, 👎 No, 🤷 Abstain, ❓ None
+
+Examples:  
+`✨🎭 • ⏳1❓ • 1c1f9765`: New Performer - Pending with 1 upvote, I have not voted  
+`✨🎞️ • ⏳1👍 • cab039d4`: New Scene - Pending with 1 upvote, I have upvoted  
+
+[stashdb-relative-date](https://github.com/feederbox826/plugins/raw/main/userscript/stashdb-relative-date.user.js)
+- adds relative dates to stashDB in Year, Month, (Week), Day
+
 [twitter-media-unblur](https://github.com/feederbox826/plugins/raw/main/userscript/twitter-media-unblur.user.js)
 - unblurs all twitter media posts as you scroll
---
+++ userscript/stashdb-link-chip.user.js
@@ -0,0 +1,225 @@
+// ==UserScript==
+// @name         stashdb-link-chip
+// @namespace    feederbox826
+// @version      0.1
+// @description  add chips to links in edit queue
+// @author       feederbox826
+// @match        https://fansdb.cc/*
+// @match        https://stashdb.org/*
+// @run-at       document-idle
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+  .EditComment a.link-chip {
+      border-radius: 3px;
+      background: #444;
+      padding: 2px;
+  }
+`);
+
+let paginationObserved = false;
+let myID = null;
+
+// wait for visible key elements
+function wfke(selector, callback) {
+var el = document.querySelector(selector);
+if (el) return callback(el);
+setTimeout(wfke, 100, selector, callback);
+}
+
+async function gqlClient(query, id) {
+return await fetch("https://stashdb.org/graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ query, variables: { id } }),
+})
+  .then((res) => res.json())
+  .then((data) => Object.values(data.data)[0]);
+}
+
+const targetType = {
+TAG: "🏷️",
+SCENE: "🎞️",
+STUDIO: "🎬",
+PERFORMER: "🎭",
+};
+const operationType = {
+CREATE: "✨",
+MODIFY: "🛠️",
+DELETE: "🗑️",
+MERGE: "🔗",
+};
+const statusType = {
+PENDING: "⏳",
+ACCEPTED: "✅",
+IMMEDIATE_ACCEPT: "✅",
+REJECTED: "❌",
+IMMEDIATE_REJECTED: "❌",
+CANCELLED: "🚫",
+FAILED: "⚠️",
+};
+
+const voteType = {
+ACCEPT: "👍",
+REJECT: "👎",
+ABSTAIN: "🤷",
+NONE: "❓",
+};
+
+const whoami = () =>
+gqlClient(`query { me { id } }`, "").then((data) => (myID = data.id));
+
+async function lookupEdit(editID) {
+// fetch edit
+const query = `query ($id: ID!) {
+  findEdit(id: $id) { target_type operation vote_count status
+  votes { user { id } vote
+}}}`;
+const edit = await gqlClient(query, editID);
+// parse edit
+const target = targetType[edit.target_type];
+const operation = operationType[edit.operation];
+const vote = edit.vote_count;
+const status = statusType[edit.status];
+const myVote = vote == 0 ? false : edit.votes.find((v) => v.user.id == myID);
+const myVoteType = myVote ? voteType[myVote.vote] : voteType.NONE;
+const voteString =
+  edit.status !== "PENDING" ? status : ` ${status}${vote}${myVoteType}`;
+// return chip data
+return `${operation}${target} • ${voteString} • ${editID.split("-")[0]} `;
+}
+
+async function lookupScene(sceneID) {
+// fetch scene
+const query = `query ($id: ID!) {
+      findScene(id: $id) {
+      date title
+      studio { name }
+      fingerprints { submissions }
+  }}`;
+const data = await gqlClient(query, sceneID);
+const fingerprintCount = data.fingerprints.length;
+const totalFingerprints = data.fingerprints.reduce(
+  (acc, cur) => acc + cur.submissions,
+  0,
+);
+return `${data.title} (${data.date}) • ${data.studio.name} | ${fingerprintCount} FPs / ${totalFingerprints} Total `;
+}
+
+async function lookupSceneDelete(sceneID) {
+// fetch scene
+const query = `query ($id: ID!) {
+      findScene(id: $id) {
+      fingerprints { submissions }
+  }}`;
+const data = await gqlClient(query, sceneID);
+const fingerprintCount = data.fingerprints.length;
+const totalFingerprints = data.fingerprints.reduce(
+  (acc, cur) => acc + cur.submissions,
+  0,
+);
+return `${fingerprintCount} FPs / ${totalFingerprints} Total `;
+}
+
+async function lookupStudioDelete(studioID) {
+// fetch studio
+const query = `query ($id: ID!) {
+  queryScenes(input: { studios: { value: [$id], modifier: INCLUDES } }) {
+      count
+  }}`;
+const studio = await gqlClient(query, studioID);
+return `${studio.count} scenes`;
+}
+
+async function lookupPerformerDelete(performerID) {
+const query = `query ($id: ID!) { findPerformer(id: $id) { scene_count }}`;
+const performer = await gqlClient(query, performerID);
+return `${performer.scene_count} scenes`;
+}
+
+const prependOrReplace = (chip, link) => {
+if (link.classList.contains("link-chip")) return;
+link.classList.add("link-chip");
+const oldText = link.textContent;
+// if just link, replace
+if (oldText == link.href) return (link.textContent = chip);
+// otherwise prepend chip
+link.textContent = `${oldText} (${chip})`;
+};
+
+const appendChip = (chip, link) => {
+if (link.classList.contains("link-chip")) return;
+link.classList.add("link-chip");
+link.textContent += ` | ${chip}`;
+};
+
+function searchLink(elem) {
+// iterate through deletion targets
+const delTargets = elem.querySelectorAll(".col>.bg-danger");
+for (const target of delTargets) {
+  const targetLink = target.querySelector("a:not(.link-chip)");
+  if (!targetLink || targetLink.classList.contains("link-chip")) continue;
+  // check for scene links
+  if (targetLink.href.includes("/scenes/")) {
+    lookupSceneDelete(targetLink.href.split("/").pop()).then((chip) =>
+      appendChip(chip, targetLink),
+    );
+  } else if (targetLink.href.includes("/studios/")) {
+    lookupStudioDelete(targetLink.href.split("/").pop()).then((chip) =>
+      appendChip(chip, targetLink),
+    );
+  } else if (targetLink.href.includes("/performers/")) {
+    lookupPerformerDelete(targetLink.href.split("/").pop()).then((chip) =>
+      appendChip(chip, targetLink),
+    );
+  }
+}
+// iterate through comments
+const commentLinks = elem.querySelectorAll(".EditComment a:not(.link-chip)");
+for (const link of commentLinks) {
+  if (link.classList.contains("link-chip")) continue;
+  // check for guideline links
+  if (link.href.includes("guidelines.stashdb.org")) {
+    prependOrReplace("📚", link);
+    // check for edit links
+  } else if (link.href.includes("/edits/") && !link.href.includes("#")) {
+    lookupEdit(link.href.split("/").pop()).then((chip) =>
+      prependOrReplace(chip, link),
+    );
+    // check for scene links
+  } else if (link.href.includes("/scenes/")) {
+    lookupScene(link.href.split("/").pop().split("#")[0]).then((chip) =>
+      prependOrReplace(chip, link),
+    );
+  }
+}
+}
+function observePagination() {
+if (paginationObserved) return;
+// pagination observer
+new MutationObserver(() => wfkeChips()).observe(
+  document.querySelector("ul.pagination"),
+  { attributes: true, subtree: true },
+);
+paginationObserved = true;
+}
+const wfkeChips = () => wfke(".EditCard", addChips);
+function addChips() {
+console.log("addChips");
+paginationObserved = false;
+for (const elem of document.querySelectorAll(".EditCard")) {
+  searchLink(elem);
+}
+}
+// change observer
+new MutationObserver(() => {
+console.log("runObserver");
+paginationObserved = false;
+wfkeChips();
+wfke("ul.pagination", observePagination);
+}).observe(document.querySelector("title"), { childList: true });
+wfkeChips();
+whoami();
